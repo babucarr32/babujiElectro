49d60e8c678b0cc315a606c4e52019843b910f6b
babucarr32 <baboucarry@gmail.com>
2022-02-13 13:57:34 +0100

    Added index.html file

--- Babujii/settings.py
+++ Babujii/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'Babujii.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "templates")],
+        'DIRS': ["C:\Users\User\babujii\Babujii\app\Templates\home.html"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
