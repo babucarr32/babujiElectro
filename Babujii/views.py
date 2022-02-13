4ef0f09a4d081453907a75f366cae87226304137
babucarr32 <baboucarry@gmail.com>
2022-02-13 13:04:34 +0100

    Added index.html file

--- Babujii/views.py
+++ Babujii/views.py
@@ -1,11 +1,4 @@
 from django.shortcuts import render
 
 def homePage(request):
-    databaseInfo = "omeModel.objects"
-    i = 10
-    return render(request, 
-                "index.html",
-                {"jobs": databaseInfo,
-                "x": i},
-                
-                )
+    return render(request,"index.html")
--- Home/views.py
+++ Home/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+# from django.shortcuts import render
+# from django.http import HttpResponse
 
-def homePage(request,):
-    databaseInfo = "homeModel.objects"
-    i = 10
-    return render(request, HttpResponse("Hello world")
-                )
+# def homePage(request,):
+#     databaseInfo = "homeModel.objects"
+#     i = 10
+#     return render(request, HttpResponse("Hello world")
+#                 )
