5506afaecc72ab3b48c68525563e0f8e33f23cd5
babucarr32 <baboucarry@gmail.com>
2022-02-13 15:15:02 +0100

    Added index.html file

--- app/Babujii/settings.py
+++ app/Babujii/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(1ijfgp-tatu!ik)#fjr4x0qsox0%a@fgc-5d+xjr%%95_bk=!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['babuji-electronics.herokuapp.com']
 
 
 # Application definition
