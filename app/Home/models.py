6a3e8c93e0a8782d01bee15f891d5488ed00ffc6
babucarr32 <baboucarry@gmail.com>
2022-02-14 08:33:47 +0100

    Added products to my web

--- app/Home/models.py
+++ app/Home/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 class JobsConfig(models.Model):
     image = models.ImageField(upload_to='images/')
-    summary = models.CharField(max_length=200)
     name = models.CharField(max_length=200)
     price = models.IntegerField()
     detail = models.TextField( default= "")
