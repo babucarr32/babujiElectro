f4eb415e64a0c0840ec028ddd3bef799f878309a
babucarr32 <baboucarry@gmail.com>
2022-02-27 20:14:29 +0100

    Edited the search functionality

--- app/Babujii/settings.py
+++ app/Babujii/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(1ijfgp-tatu!ik)#fjr4x0qsox0%a@fgc-5d+xjr%%95_bk=!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
